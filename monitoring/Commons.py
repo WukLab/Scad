5e0ac7e7d3fd538aa784f0e22196c5af8dbb6671
mohammad shahrad <engshahrad@gmail.com>
2021-04-25 20:55:19 +0000

    new formatting for the table

--- monitoring/Commons.py
+++ monitoring/Commons.py
@@ -4,8 +4,8 @@ def CreatePerfDB(db_filename):
     conn = sqlite3.connect(db_filename)
     conn.execute('''CREATE TABLE PERFPROF
          (NAME INT TEXT KEY     NOT NULL,
-         ID           TEXT    NOT NULL,
-         TIMESTAMP      REAL     NOT NULL,
+         POINT           TEXT    NOT NULL,
          FIELD        TEXT      NOT NULL,
-         VALUE          REAL    NOT NULL);''')
+         VALUE          REAL    NOT NULL,
+         LOGTIME      TEXT     NOT NULL);''')
     conn.close()
--- monitoring/DBQuery.py
+++ monitoring/DBQuery.py
@@ -9,11 +9,11 @@ from config import *
 if (os.path.isfile(PERF_DB_FILENAME)):
     conn = sqlite3.connect(PERF_DB_FILENAME)
     s = time.time()
-    cursor = conn.execute("SELECT NAME, ID, TIMESTAMP, FIELD, VALUE from PERFPROF")
+    cursor = conn.execute("SELECT NAME, POINT, FIELD, VALUE, LOGTIME from PERFPROF")
     e = time.time()
     print('Query execution time (s): ' + str(e - s) + '\n')
     for row in cursor:
-        print(str(row) + "\n")
+        print(str(row))
     conn.close()
 else:
     print('Error: No profiling DB exists to be read!')
