96e7447c4b4b1a54970ee6abc923cc2c9adc98a6
Zac Blanco <zacdblanco@gmail.com>
2021-05-03 13:53:09 -0700

    Swap API now returns activationID

--- openwhisk/core/controller/src/main/scala/org/apache/openwhisk/core/controller/Swap.scala
+++ openwhisk/core/controller/src/main/scala/org/apache/openwhisk/core/controller/Swap.scala
@@ -1,7 +1,8 @@
 package org.apache.openwhisk.core.controller
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes.{InternalServerError, OK}
 import akka.http.scaladsl.server.{Directive, Directive1, Directives, RequestContext, Route, RouteResult}
 import org.apache.openwhisk.common.{Logging, TransactionId}
 import org.apache.openwhisk.core.connector.ActivationMessage
@@ -14,6 +15,7 @@ import org.apache.openwhisk.core.topbalancer.SwapObject
 import spray.json.JsObject
 
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object Swap {
 
@@ -73,8 +75,10 @@ trait SwapApi extends Directives with AuthorizedRouteProvider with Authenticated
             entity(as[JsObject]) { swap =>
               val swapObject = SwapObject.serdes.read(swap)
               logging.debug(this, s"scheduling new swap for $name:$swapObject")
-              scheduleSwap(swapObject, user)
-              complete(OK)
+              onComplete(scheduleSwap(swapObject, user)) {
+                case Success(id) => complete(OK, id.toJsObject)
+                case Failure(exception) => complete(InternalServerError, exception)
+              }
             }
           case _ => reject
         }
@@ -98,15 +102,20 @@ trait SwapApi extends Directives with AuthorizedRouteProvider with Authenticated
     )
   }
 
-  protected def scheduleSwap(obj: SwapObject, user: Identity)(implicit transid: TransactionId): Future[Unit] = {
-    SwapObject.swapAction(obj.mem) flatMap { exec =>
-      WhiskActionMetaData.serdes.read(WhiskAction.serdes.write(exec)).toExecutableWhiskAction flatMap { action =>
+  protected def scheduleSwap(obj: SwapObject, user: Identity)(implicit transid: TransactionId): Future[ActivationId] = {
+    val z: Future[ActivationId] = SwapObject.swapAction(obj.mem) flatMap { exec =>
+      val y = WhiskActionMetaData.serdes.read(WhiskAction.serdes.write(exec)).toExecutableWhiskAction flatMap { action =>
         val msg = activationMsgFromObj(obj, user)
         logging.debug(this, s"scheduling swap activation for ${obj.appActivationId}::${obj.functionActivationId} with activation ${msg.activationId}")
-        Some(loadBalancer.publish(action, msg))
+        val x: Future[ActivationId] = loadBalancer.publish(action, msg).flatten.flatMap({
+          case Left(id) => Future.successful(id)
+          case Right(act) => Future.successful(act.activationId)
+        })
+        Some(x)
       }
-    }
-    Future.successful(())
+      y
+    } getOrElse Future.failed(new Exception("Fails to get swap action"))
+    z
   }
 
 }
