5b0794dcd3827d48dd7e6f63ed1a072479573f4a
Zac Blanco <zacdblanco@gmail.com>
2021-12-03 23:55:48 -0800

    update wskgen for `withMerged` param

--- tests/dags/local-mem/obj0.o.py
+++ tests/dags/local-mem/obj0.o.py
@@ -1,5 +1,21 @@
 #@ type: compute
-#@ withMemory: true
+#@ withMerged:
+#@   -
+#@    action: ""
+#@    resources:
+#@      compute: 1.0
+#@      memory: 1024
+#@      storage: 1024
+#@    elem: Compute
+#@   -
+#@    action: ""
+#@    resources:
+#@      compute: 1.0
+#@      memory: 4096
+#@      storage: 1024
+#@    elem: Memory
+#@
+
 import numpy as np
 import disaggrt.buffer_pool_lib as buffer_pool_lib
 from disaggrt.rdma_array import remote_array
@@ -7,20 +23,20 @@ from disaggrt.rdma_array import remote_array
 def main(params, action):
     df = np.array([1,2,3])
     # build transport
-    trans_name = 'test'
+    trans_name = 'memory'
     trans = action.get_transport(trans_name, 'rdma')
     trans.reg(buffer_pool_lib.buffer_size)
 
     # write back
     buffer_pool = buffer_pool_lib.buffer_pool({trans_name:trans})
     rdma_array = remote_array(buffer_pool, input_ndarray=df, transport_name=trans_name)
-    # print(f"[tpcds] {tag_print}: finish writing back")
 
     # transfer the metedata
     context_dict = {}
     context_dict["df"] = rdma_array.get_array_metadata()
     context_dict["bp"] = buffer_pool.get_buffer_metadata()
+    print('returning')
     return {
         'meta': context_dict,
-        'result': df
+        'result': [1, 2, 3]
     }
